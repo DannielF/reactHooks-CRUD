f9efcf20e0a626859348916b1db8c8c977a004ba
DannielF <danielgranados1992@gmail.com>
2022-04-29 18:16:42 -0500

    [Add] localStorage -- initialValues with userData

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,20 +1,36 @@
-import { UserTable } from '../components/UserTable'
+import { UserTable } from '../components/UserTable';
+import { useLocalStorage } from './utils/useLocalStorage';
 
 function App() {
+
+  const userData = [
+    { id: 1, name: 'John Doe', username: 'jdoe'},
+    { id: 2, name: 'Jane Doe', username: 'jdoe2'},
+    { id: 3, name: 'John Smith', username: 'jsmith'},
+  ]
+
+  // hook to use local storage
+  const {
+    item: users,
+    saveItem: saveUsers,
+    loading,
+    error,
+  } = useLocalStorage('users', userData);
+
   return (
     <div className="container">
       <h1>CRUD Api with hooks</h1>
-      <div className='flex-row'>
-        <div className='flex-large'>
+      <div className="flex-row">
+        <div className="flex-large">
           <h2>Add user</h2>
         </div>
-        <div className='flex-large'>
+        <div className="flex-large">
           <h2>View users</h2>
-          <UserTable/>
+          <UserTable users={users} />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
